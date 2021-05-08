86da7809c7a4c2ff0a14bf7d071c54e08b5f9f4c
zakirabdulla <zakirabdullayev8@gmail.com>
2021-05-08 20:53:28 +0400

    create models

---
+++ models.py
@@ -0,0 +1,35 @@
+from app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.filter_by(id = id).first()
+
+class CustomMixin():
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class User(UserMixin,CustomMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(30),nullable=False)
+    password = db.Column(db.String(255),nullable=False)
+    email = db.Column(db.String(50),nullable=False)
+    links = db.relationship('Link', backref='user', lazy=True)
+    socials = db.relationship('Social', backref='user', lazy=True)
+
+class Link(CustomMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    type = db.Column(db.String(5),nullable=False)
+    address = db.Column(db.String(255),nullable=False)
+    title = db.Column(db.String(50),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id',ondelete='CASCADE'))
+
+class Social(CustomMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    site = db.Column(db.String(5),nullable=False)
+    address = db.Column(db.String(255),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id',ondelete='CASCADE'))
